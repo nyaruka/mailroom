74b132f4a00898861fddab8b508d6b45b70cdfc1
Rowan Seymour <rowanseymour@gmail.com>
2025-05-16 17:50:53 -0500

    Fix comment

--- core/runner/modifiers.go
+++ core/runner/modifiers.go
@@ -11,13 +11,12 @@ import (
 	"github.com/nyaruka/mailroom/runtime"
 )
 
-// ApplyModifiers modifies contacts by applying modifiers and handling the resultant events.
+// ApplyModifiers modifies contacts by applying modifiers and processing the resultant events.
 //
 // Note we don't load the user object from org assets as it's possible that the user isn't part of the org, e.g. customer support.
 func ApplyModifiers(ctx context.Context, rt *runtime.Runtime, oa *models.OrgAssets, userID models.UserID, modifiersByContact map[*flows.Contact][]flows.Modifier) (map[*flows.Contact][]flows.Event, error) {
 	// create an environment instance with location support
 	env := flows.NewAssetsEnvironment(oa.Env(), oa.SessionAssets())
-
 	eng := goflow.Engine(rt)
 
 	eventsByContact := make(map[*flows.Contact][]flows.Event, len(modifiersByContact))
