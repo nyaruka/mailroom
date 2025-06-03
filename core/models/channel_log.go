59b0f2751a3ad7923302ba1f94bff279b3a2caab
Rowan Seymour <rowanseymour@gmail.com>
2025-06-03 11:35:57 -0500

    Stop writing channel logs to database and add is_error data field

--- core/models/channel_log.go
+++ core/models/channel_log.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log/slog"
 	"slices"
@@ -13,13 +12,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/nyaruka/gocommon/aws/dynamo"
 	"github.com/nyaruka/gocommon/httpx"
-	"github.com/nyaruka/gocommon/jsonx"
 	"github.com/nyaruka/mailroom/runtime"
 	"github.com/nyaruka/mailroom/utils/clogs"
 )
-
-// ChannelLogID is our type for a channel log id
-type ChannelLogID int64
 
 const (
 	ChannelLogTypeIVRStart    clogs.Type = "ivr_start"
@@ -35,8 +30,7 @@ const (
 type ChannelLog struct {
 	*clogs.Log
 
-	channel  *Channel
-	attached bool
+	channel *Channel
 }
 
 // NewChannelLog creates a new channel log with the given type and channel
@@ -54,21 +48,6 @@ func newChannelLog(t clogs.Type, ch *Channel, r *httpx.Recorder, redactVals []st
 		Log:     clogs.New(t, r, redactVals),
 		channel: ch,
 	}
-}
-
-// if we have an error or a non 2XX/3XX http response then log is considered an error
-func (l *ChannelLog) isError() bool {
-	if len(l.Errors) > 0 {
-		return true
-	}
-
-	for _, l := range l.HttpLogs {
-		if l.StatusCode < 200 || l.StatusCode >= 400 {
-			return true
-		}
-	}
-
-	return false
 }
 
 func (l *ChannelLog) DynamoKey() runtime.DynamoKey {
@@ -96,27 +75,10 @@ func (l *ChannelLog) DynamoItem() (*runtime.DynamoItem, error) {
 			"type":       l.Type,
 			"elapsed_ms": int(l.Elapsed / time.Millisecond),
 			"created_on": l.CreatedOn,
+			"is_error":   l.IsError(),
 		},
 		DataGZ: dataGZ,
 	}, nil
-}
-
-const sqlInsertChannelLog = `
-INSERT INTO channels_channellog( uuid,  channel_id,  log_type,  http_logs,  errors,  is_error,  elapsed_ms,  created_on)
-                         VALUES(:uuid, :channel_id, :log_type, :http_logs, :errors, :is_error, :elapsed_ms, :created_on)
-  RETURNING id`
-
-// channel log to be inserted into the database
-type dbChannelLog struct {
-	ID        ChannelLogID    `db:"id"`
-	UUID      clogs.UUID      `db:"uuid"`
-	ChannelID ChannelID       `db:"channel_id"`
-	Type      clogs.Type      `db:"log_type"`
-	HTTPLogs  json.RawMessage `db:"http_logs"`
-	Errors    json.RawMessage `db:"errors"`
-	IsError   bool            `db:"is_error"`
-	ElapsedMS int             `db:"elapsed_ms"`
-	CreatedOn time.Time       `db:"created_on"`
 }
 
 // InsertChannelLogs writes the given channel logs to the db
@@ -147,31 +109,6 @@ func InsertChannelLogs(ctx context.Context, rt *runtime.Runtime, logs []*Channel
 		if len(resp.UnprocessedItems) > 0 {
 			// TODO shouldn't happend.. but need to figure out how we would retry these
 			slog.Error("unprocessed items writing logs to dynamo", "count", len(resp.UnprocessedItems))
-		}
-	}
-
-	unattached := make([]*dbChannelLog, 0, len(logs))
-
-	for _, l := range logs {
-		if !l.attached {
-			// if log isn't attached to a message or call we need to write it to the db so that it's retrievable
-			unattached = append(unattached, &dbChannelLog{
-				UUID:      l.UUID,
-				ChannelID: l.channel.ID(),
-				Type:      l.Type,
-				HTTPLogs:  jsonx.MustMarshal(l.HttpLogs),
-				Errors:    jsonx.MustMarshal(l.Errors),
-				IsError:   l.isError(),
-				CreatedOn: l.CreatedOn,
-				ElapsedMS: int(l.Elapsed / time.Millisecond),
-			})
-		}
-	}
-
-	if len(unattached) > 0 {
-		err := BulkQuery(ctx, "insert channel log", rt.DB, sqlInsertChannelLog, unattached)
-		if err != nil {
-			return fmt.Errorf("error inserting unattached channel logs: %w", err)
 		}
 	}
 
