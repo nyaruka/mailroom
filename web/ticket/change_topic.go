d40b8e5d5353effe53721eb5e4edf319c796fc13
Rowan Seymour <rowanseymour@gmail.com>
2025-09-10 14:45:20 -0500

    Use topic UUID in ticket/change_topic requests

--- web/ticket/change_topic.go
+++ web/ticket/change_topic.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/nyaruka/goflow/assets"
 	"github.com/nyaruka/goflow/flows"
 	"github.com/nyaruka/goflow/flows/events"
 	"github.com/nyaruka/goflow/flows/modifiers"
@@ -21,7 +22,7 @@ func init() {
 type changeTopicRequest struct {
 	bulkTicketRequest
 
-	TopicID models.TopicID `json:"topic_id" validate:"required"`
+	TopicUUID assets.TopicUUID `json:"topic_uuid" validate:"required"`
 }
 
 // Changes the topic of the tickets with the given ids
@@ -30,7 +31,7 @@ type changeTopicRequest struct {
 //	  "org_id": 123,
 //	  "user_id": 234,
 //	  "ticket_uuids": ["01992f54-5ab6-717a-a39e-e8ca91fb7262", "01992f54-5ab6-725e-be9c-0c6407efd755"],
-//	  "topic_id": 345
+//	  "topic_uuid": "7e39a04f-c7e9-4c1b-b9eb-7f3b4be5f183"
 //	}
 func handleChangeTopic(ctx context.Context, rt *runtime.Runtime, r *changeTopicRequest) (any, int, error) {
 	oa, err := models.GetOrgAssets(ctx, rt, r.OrgID)
@@ -38,9 +39,10 @@ func handleChangeTopic(ctx context.Context, rt *runtime.Runtime, r *changeTopicR
 		return nil, 0, fmt.Errorf("unable to load org assets: %w", err)
 	}
 
-	// TODO have RP pass topic by UUID to simplify this
-	tp := oa.TopicByID(r.TopicID)
-	topic := oa.SessionAssets().Topics().Get(tp.UUID())
+	topic := oa.SessionAssets().Topics().Get(r.TopicUUID)
+	if topic == nil {
+		return nil, 0, fmt.Errorf("no such topic: %s", r.TopicUUID)
+	}
 
 	mod := modifiers.NewTicketTopic(r.TicketUUIDs, topic)
 
