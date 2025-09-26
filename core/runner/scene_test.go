d55a4e3ab00dbfcfd538e0d3be2f06eb94cddff8
Rowan Seymour <rowanseymour@gmail.com>
2025-09-26 15:25:57 -0500

    Fix test

--- core/runner/scene_test.go
+++ core/runner/scene_test.go
@@ -70,7 +70,7 @@ func TestSessionCreationAndUpdating(t *testing.T) {
 
 	// check events were persisted to DynamoDB
 	rt.Writers.History.Flush()
-	dyntest.AssertCount(t, rt.Dynamo, "TestHistory", 4)
+	dyntest.AssertCount(t, rt.Dynamo, "TestHistory", 6)
 
 	testsuite.AssertContactFires(t, rt, testdb.Bob.ID, map[string]time.Time{
 		fmt.Sprintf("E:%s", scBob.Session.UUID()): time.Date(2025, 2, 25, 16, 55, 9, 0, time.UTC), // 10 minutes in future
