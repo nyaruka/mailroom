78b7c69234f2e2869b8ff6e1ed912feed22a0a1a
Rowan Seymour <rowanseymour@gmail.com>
2025-12-11 15:07:29 +0000

    Use docker exec instead of installing pg_restore locally

--- testsuite/runtime.go
+++ testsuite/runtime.go
@@ -22,10 +22,11 @@ import (
 )
 
 const (
-	elasticURL           = "http://elastic:9200"
-	elasticContactsIndex = "test_contacts"
-	postgresDumpPath     = "./testsuite/testdata/postgres.dump"
-	dynamoTablesPath     = "./testsuite/testdata/dynamo.json"
+	elasticURL            = "http://elastic:9200"
+	elasticContactsIndex  = "test_contacts"
+	postgresContainerName = "textit-postgres-1"
+	postgresDumpPath      = "./testsuite/testdata/postgres.dump"
+	dynamoTablesPath      = "./testsuite/testdata/dynamo.json"
 )
 
 // Refresh is our type for the pieces of org assets we want fresh (not cached)
@@ -163,8 +164,7 @@ func loadTestDump(t *testing.T) {
 
 	defer dump.Close()
 
-	cmd := exec.Command("pg_restore", "-h", "postgres", "-U", "mailroom_test", "-d", "mailroom_test", "--no-password")
-	cmd.Env = append(os.Environ(), "PGPASSWORD=temba")
+	cmd := exec.Command("docker", "exec", "-i", "-e", "PGPASSWORD=temba", postgresContainerName, "pg_restore", "-U", "mailroom_test", "-d", "mailroom_test", "--no-password")
 	cmd.Stdin = dump
 
 	output, err := cmd.CombinedOutput()
