a959c6c78dbebdd1a8645e7344df167607d20927
Rowan Seymour <rowanseymour@gmail.com>
2026-01-15 14:55:33 +0000

    Remove code for overriding flow definitions in simulations

--- core/models/assets_test.go
+++ core/models/assets_test.go
@@ -80,28 +80,13 @@ func TestCloneForSimulation(t *testing.T) {
 	oa, err := models.GetOrgAssets(ctx, rt, testdb.Org1.ID)
 	require.NoError(t, err)
 
-	newFavoritesDef := []byte(`{
-		"uuid": "9de3663f-c5c5-4c92-9f45-ecbc09abcc85",
-		"name": "Favorites",
-		"nodes": []
-	}`)
-
-	newDefs := map[assets.FlowUUID][]byte{
-		testdb.Favorites.UUID: []byte(newFavoritesDef),
-	}
-
 	testChannels := []assets.Channel{
 		static.NewChannel("d7be3965-4c76-4abd-af78-ebc0b84ab621", "Test Channel 1", "1234567890", []string{"tel"}, nil, nil),
 		static.NewChannel("fd130d20-65f8-43fc-a3c5-a3fa4d1e4193", "Test Channel 2", "2345678901", []string{"tel"}, nil, nil),
 	}
 
-	clone, err := oa.CloneForSimulation(ctx, rt, newDefs, testChannels)
+	clone, err := oa.CloneForSimulation(ctx, rt, testChannels)
 	require.NoError(t, err)
-
-	// should get new definition
-	flow, err := clone.FlowByUUID(testdb.Favorites.UUID)
-	require.NoError(t, err)
-	assert.Equal(t, string(newFavoritesDef), string(flow.Definition()))
 
 	// test channels should be accesible to engine
 	testChannel1 := clone.SessionAssets().Channels().Get("d7be3965-4c76-4abd-af78-ebc0b84ab621")
@@ -113,16 +98,7 @@ func TestCloneForSimulation(t *testing.T) {
 	vonage := clone.SessionAssets().Channels().Get(testdb.VonageChannel.UUID)
 	assert.Equal(t, "Vonage", vonage.Name())
 
-	// original assets still has original flow definition
-	flow, err = oa.FlowByUUID(testdb.Favorites.UUID)
-	require.NoError(t, err)
-	assert.Equal(t, "{\"_ui\": {\"nodes\": {\"10c9c241-777f-4010-a841-6e87abed8520\": {\"typ", string(flow.Definition())[:64])
-
-	// and doesn't have the test channels
+	// original assets doesn't have the test channels
 	testChannel1 = oa.SessionAssets().Channels().Get("d7be3965-4c76-4abd-af78-ebc0b84ab621")
 	assert.Nil(t, testChannel1)
-
-	// can't override definition for a non-existent flow
-	_, err = oa.CloneForSimulation(ctx, rt, map[assets.FlowUUID][]byte{"a121f1af-7dfa-47af-9d22-9726372e2daa": newFavoritesDef}, nil)
-	assert.EqualError(t, err, "unable to find flow with UUID 'a121f1af-7dfa-47af-9d22-9726372e2daa': not found")
 }
